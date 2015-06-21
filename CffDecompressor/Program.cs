c6e9226ec26093eea11299f43e591cdafc2d5a36
Koen J <mail@koenj.com>
2015-06-21 16:26:01 +0200

    Fixed a compiler error.

--- CffDecompressor/Program.cs
+++ CffDecompressor/Program.cs
@@ -11,7 +11,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
-﻿using CffDecompressor.BFCardView;
 using System;
 using System.Collections.Generic;
 using System.IO;
